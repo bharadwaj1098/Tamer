ce7a252398f0ea93b53cc904b87a1af9c73d2b83
bharadwaj1098 <bharadwaj.reddy98@gmail.com>
2021-03-06 16:44:22 -0500

    encoder done

--- Deep_Auto.py
+++ Deep_Auto.py
@@ -4,41 +4,29 @@ import torch.nn.functional as F
 import torch.optim as optim 
 
 class encoder(nn.Module):
-    '''
     def __init__(self):
         super(encoder, self).__init__()
-        self.conv1 = nn.Conv2d(1, 64, 3)
+        
+        self.conv1 = nn.Conv2d(3, 64, 3)
         self.conv2 = nn.Conv2d(64, 64, 3)
         self.conv3 = nn.Conv2d(64, 1, 3)
 
         self.conv_bn1 = nn.BatchNorm2d(64)
         self.conv_bn2 = nn.BatchNorm2d(1)
-        '''
-    def __init__(self):
-        super(encoder, self).__init__()
-        self.cnn = nn.Sequential(
-            nn.Conv2d(1, 64, 3),
-            nn.BatchNorm2d(64),
-            nn.MaxPool2d(2),
-            nn.ReLU(),
 
-            nn.Conv2d(64, 64, 3),
-            nn.BatchNorm2d(64),
-            nn.MaxPool2d(2),
-            nn.ReLU(),
+        def conv2d_size_out(size, kernel_size=3, stride=1):
+            return (size - (kernel_size - 1) - 1) // stride  + 1
 
-            nn.Conv2d(64, 64, 3),
-            nn.BatchNorm2d(64),
-            nn.MaxPool2d(2),
-            nn.ReLU(),
-'''
-            nn.Conv2d(64, 1, 3),
-            nn.BatchNorm2d(1),
-            nn.MaxPool2d(2),
-            nn.ReLU()
-        )
+        def max2d_size_out(size, kernel_size = 2, stride = 2):
+            return (size - (kernel_size - 1) - 1) // stride  + 1 
+
+        linear_input_size = max2d_size_out(conv2d_size_out(max2d_size_out( conv2d_size_out(160))))
+        linear_input_size = max2d_size_out(conv2d_size_out(max2d_size_out( conv2d_size_out(linear_input_size))))
+
+        self.linear_1 = nn.Linear(linear_input_size^2, 100)
+
     def forward(self, x):
-
+        x = x
         x = F.max_pool2d( self.conv_bn1(self.conv1(x)), 2)
         x = F.relu(x)
 
@@ -51,12 +39,12 @@ class encoder(nn.Module):
         x = F.max_pool2d( self.conv_bn2(self.conv3(x)), 2)
         x = F.relu(x) 
         
+        x = F.relu(self.linear_1(x.view(x.size(0), -1)))
+
         return x
 
-encoder = encoder()
 
-print(encoder)
-'''
-for i in encoder.parameters():
-    print(i.shape)
-'''
+class decoder(nn.module):
+    
+    def __init__(self):
+        super(decoder, self).__init()
