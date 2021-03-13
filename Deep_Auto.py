d5223a8ce8dc18560021ccefed45bce7bff9b806
bharadwaj1098 <bharadwaj.reddy98@gmail.com>
2021-03-13 17:31:09 -0500

    nothing major, added notebooks

--- Deep_Auto.py
+++ Deep_Auto.py
@@ -3,7 +3,8 @@ import numpy as np
 import time
 import random
 import cv2
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
+from collections import namedtuple
 import warnings 
 warnings.filterwarnings("ignore") 
 
@@ -23,9 +24,11 @@ class encoder(nn.Module):
 
         self.conv_bn1 = nn.BatchNorm2d(64)
         self.conv_bn2 = nn.BatchNorm2d(1)
-
+        
         self.linear_1 = nn.Linear(64, 100)
 
+        self.optimizer = optim.Adam(self.parameters(), lr= 0.001)  
+        
     def forward(self, x):
         x = x
         x = F.max_pool2d(self.conv_bn1(self.conv1(x)), 2)
@@ -33,7 +36,7 @@ class encoder(nn.Module):
         x = F.max_pool2d(self.conv_bn1(self.conv2(x)), 2)
         x = F.max_pool2d(self.conv_bn2(self.conv3(x)), 2)
         x = x.view(x.size(0), -1)
-        x = F.relu(self.linear_1(x))
+        x = self.linear_1(x) #encoded states might come in "-ve" so no Relu or softmax
         return x
 
 class decoder(nn.Module):
@@ -53,6 +56,8 @@ class decoder(nn.Module):
         self.deconv_bn1 = nn.BatchNorm2d(64)
         self.deconv_bn2 = nn.BatchNorm2d(3)  
 
+        self.optimizer = optim.Adam(self.parameters(), lr= 0.001)
+
     def forward(self,x):
         x = x
         x = F.relu(self.linear_1(x))  
@@ -62,7 +67,7 @@ class decoder(nn.Module):
         x = self.deconv_bn1(self.deconv_2(self.upsample_2(x)))
         x = self.deconv_bn2(self.deconv_3(self.upsample_1(x))) 
         return x
-'''
+
 class policynet(nn.Module):
     def __init__(self):
         super(policynet, self).__init__()
@@ -78,6 +83,8 @@ class policynet(nn.Module):
         self.linear_2 = nn.Linear(100,16)
         self.linear_3 = nn.Linear(16,4)
 
+        self.optimizer = optim.Adam(self.parameters(), lr= 0.001)
+
     def forward(self, x):
         x = x
         x = F.max_pool2d(self.conv_bn1(self.conv1(x)), 2) 
@@ -85,102 +92,115 @@ class policynet(nn.Module):
         x = F.max_pool2d(self.conv_bn1(self.conv2(x)), 2) 
         x = F.max_pool2d(self.conv_bn2(self.conv3(x)), 2) 
         x = x.view(x.size(0), -1)
-        x = F.relu(self.linear_1(x))
-        x = F.relu(self.linear_2(x))
-        x = F.relu(self.linear_3(x))
+        x = self.linear_1(x)
+        x = self.linear_2(x)
+        x = self.linear_3(x)
         return x
-'''
 
-class Random_Agent:
-    def __init__(self, env, device):
+Experience = namedtuple(
+    'Experience',
+    ('state', 'action', 'next_state', 'reward')
+)
 
-        self.current_step = 0
-        self.env = gym.make(env)
-        self.env.reset()
-        self.current_screen = None
-        self.done = False
-        self.device = device
-        self.action_space = self.env.action_space.n
-
-    def reset(self):
-        self.env.reset()
-        self.current_screen = None
+class ReplayMemory():
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+        self.push_count = 0
     
-    def close(self):
-        self.env.close()
-
-    def render(self, mode='human'):
-        return self.env.render(mode)
-    
-    def num_actions_available(self):
-        return self.env.action_space.n
-    
-    def select_action(self):
-        action = random.randrange(self.action_space)
-        action = torch.tensor([action]).to(device)
-        return action
-    
-    def just_starting(self):
-        return self.current_screen is None
-
-    def take_action(self, action):        
-        _, reward, self.done, _ = self.env.step(action.item())
-        #return torch.tensor([reward], device=self.device)
-        return self.done
-
-    def get_state(self):
-        if self.just_starting() or self.done:
-            self.current_screen = self.get_processed_screen()
-            black_screen = torch.zeros_like(self.current_screen)
-            return black_screen
+    def push(self, experience):
+        if len(self.memory) < self.capacity:
+            self.memory.append(experience)
         else:
-            s1 = self.current_screen
-            s2 = self.get_processed_screen()
-            self.current_screen = s2
-            return s2 - s1
+            self.memory[self.push_count % self.capacity] = experience
+        self.push_count +=1 
     
-    def get_processed_screen(self):
-        screen = self.render('rgb-array').transpose((2, 0, 1)) 
-        screen = self.transform_screen(screen)
-        return screen  
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size) 
+    
+    def can_provide_sample(self, batch_size):
+        return len(self.memory) >= batch_size
 
-    def transform_screen(self, screen):
-        screen = np.ascontiguousarray(screen, dtype=np.float32) / 255 
-        screen = torch.numpy(screen)
+class EpsilonGreedy():
+    def __init__(self, start, end, decay):
+        self.start = start
+        self.end = end 
+        self.decay = decay 
+    
+    def get_exploration_rate(self, current_step):
+        rate = self.end + (self.start - self.end) * \
+            math.exp(-1. * current_step * self.decay)
+        return rate
 
-        resize = T.Compose([
-        T.ToPILImage()
-        ,T.Resize((160, 160))
-        ,T.ToTensor()
-        ])
+class Agent():
+    def __init__(self, strategy,num_actions,device):
+        self.current_step = 0
+        self .strategy = strategy
+        self.num_actions = num_actions
+        self.device = device 
+    
+    def select_action(self, state, policy_net):
+        rate = strategy.get_exploration_rate(self.current_step)
+        self.current_step += 1
+        if rate > random.random():
+            return random.randrange(self.num_actions)
+        else:
+            with torch.no_grad():
+                return policy_net(state).argmax(dim =1).item()
 
-        screen = resize(screen).unsqueeze(0).to(self.device)
-        return screen 
+class env_manager():
+    def __init__(self, device):
+        self.env = gym.make("Bowling-v0").unwrapped
+        self.env.reset()
+        self.device = device 
+        self.done = False
+    
+    def get_state(self):
+        screen = self.render(mode = "rgb_array").transpose((2,0,1))
+        screen = np.ascontiguousarray(screen, dtype = np.float32)/255
+        screen = torch.from_numpy(screen)
+        resize = T.Compose([T.ToPILImage(),
+                            T.Resize( ??, interpolation = Image.CUBIC),
+                            T.ToTensor()])
+        screen = resize(screen).unsqueeze(0).to(self.device) 
+        
 
-if __name__ = '__main__':
+'''
+if __name__ == '__main__':
 
     no_episodes = 100000
     count = 100
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    e = encoder()
-    d = decoder()
-    agent = Random_Agent("Bowling-v0", device)
+    encoder = encoder()
+    decoder = decoder()
+    agent = Random_Agent(device)
+    decoder_optimizer = optim.RMSprop(decoder.parameters(), lr=1e-4, alpha=0.99, eps=1e-8, 
+                                weight_decay=0.0005, momentum=0.9)
+    encoder_optimizer = optim.RMSprop(decoder.parameters(), lr=1e-4, alpha=0.99, eps=1e-8, 
+                                weight_decay=0.0005, momentum=0.9) 
 
     for episode in range(no_episodes):
         agent.reset()
         state = agent.get_state()
+        state = state.cuda()
         for step in range(count):
             action = agent.select_action()
-            '''
-            having problem writing the loss function
-            '''
-            if agent.take_action(action) = True:
+            encoder_output = encoder(state.cuda())
+            decoder_output = decoder(encoser_output)
+            diff = (state - decoder_output)**2
+            diff = np.array(diff)
+            summed = np.sum(diff)
+            loss_contrastive = torch.tensor(summed/(160**2) )
+            loss_contrastive.backward()
+            optimizer.step()
+
+            if agent.take_action(action) == True:
                 agent.close()
             else:
                 next_state = agent.get_state()
-                state = next_state
-            
-        
-        "
-        here calculate the loss 
-        "
+                state = next_state.cuda() 
+
+environment = "Bowling-v0"
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+'''
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import torch 
 import numpy as np
-
+import gym
+'''
 a = torch.rand((3,160,160)) 
 b = torch.rand((3,160,160)) 
 
@@ -10,3 +11,8 @@ diff = np.array(diff)
 summed = np.sum(diff)
 err = summed/(160**2)
 print(err)
+
+'''
+
+a = 12450 % 10000
+print(a) 
